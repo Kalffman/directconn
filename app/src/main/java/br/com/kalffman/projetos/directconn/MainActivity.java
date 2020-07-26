f5517dabf1aa917a350684a9367466d7456b21ab
Jefferson Kalffman de Sousa Lacerda <jeffersonkalffman@gmail.com>
2020-07-26 17:23:06 -0400

    Pesqusia de dispositivos disponíveis na rede.

--- app/src/main/java/br/com/kalffman/projetos/directconn/MainActivity.java
+++ app/src/main/java/br/com/kalffman/projetos/directconn/MainActivity.java
@@ -1,23 +1,57 @@
 package br.com.kalffman.projetos.directconn;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.IntentFilter;
+import android.content.pm.PackageManager;
 import android.net.wifi.WifiManager;
+import android.net.wifi.p2p.WifiP2pDevice;
+import android.net.wifi.p2p.WifiP2pDeviceList;
 import android.net.wifi.p2p.WifiP2pManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import br.com.kalffman.projetos.directconn.receiver.WifiP2PBroadcastReceiver;
 import br.com.kalffman.projetos.directconn.receiver.WifiStatusBroadcastReceiver;
 
 public class MainActivity extends AppCompatActivity implements WifiStatusBroadcastReceiver.WifiStatusChangeListener,
-                                                               WifiP2PBroadcastReceiver.WifiP2PChangeListener {
+                                                                WifiP2PBroadcastReceiver.WifiP2PChangeListener {
     private WifiP2pManager p2pManager;
     private WifiP2pManager.Channel p2pChannel;
+    private BroadcastReceiver receiver;
 
     private TextView tvInfo;
+    private Button btDiscover;
+    private ListView lvP2pDevices;
+
+    private ArrayAdapter adapter;
+
+    private List<WifiP2pDevice> devices;
+    private List<String> deviceNames;
+
+    private final WifiP2pManager.ActionListener actionListener = new WifiP2pManager.ActionListener() {
+        @Override
+        public void onSuccess() {
+            tvInfo.setText("Procura inicada.");
+        }
+
+        @Override
+        public void onFailure(int reason) {
+            tvInfo.setText(String.format("Erro na procura: %d", reason));
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +66,19 @@ public class MainActivity extends AppCompatActivity implements WifiStatusBroadca
 
     private void loadComponents() {
         tvInfo = findViewById(R.id.tv_info);
+        btDiscover = findViewById(R.id.bt_discover);
+
+        btDiscover.setOnClickListener(v -> {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) return;
+
+            p2pManager.discoverPeers(p2pChannel, actionListener);
+        });
+
+        lvP2pDevices = findViewById(R.id.lv_p2p_devices);
+
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, new ArrayList<>());
+
+        lvP2pDevices.setAdapter(adapter);
     }
 
 
@@ -52,7 +99,15 @@ public class MainActivity extends AppCompatActivity implements WifiStatusBroadca
         intentFilter.addAction(WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION);
         intentFilter.addAction(WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION);
 
-        registerReceiver(new WifiP2PBroadcastReceiver(p2pManager, p2pChannel, this), intentFilter);
+        this.receiver = new WifiP2PBroadcastReceiver(p2pManager, p2pChannel, this, this);
+
+        registerReceiver(this.receiver, intentFilter);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(this.receiver);
     }
 
     @Override
@@ -61,7 +116,24 @@ public class MainActivity extends AppCompatActivity implements WifiStatusBroadca
     }
 
     @Override
-    public void onChange(String action) {
+    public void onChange(String action, List<WifiP2pDevice> devices) {
+        if (action != null)
+            tvInfo.setText(action);
 
+        updateList(devices);
+    }
+
+    private void updateList(List<WifiP2pDevice> list) {
+        devices = list;
+
+        if(devices != null && !devices.isEmpty()) {
+            deviceNames = new ArrayList<>();
+
+            for(WifiP2pDevice d : devices) deviceNames.add(d.deviceName);
+
+            adapter.clear();
+            adapter.addAll(list);
+            adapter.notifyDataSetChanged();
+        }
     }
 }
--- app/src/main/java/br/com/kalffman/projetos/directconn/receiver/WifiP2PBroadcastReceiver.java
+++ app/src/main/java/br/com/kalffman/projetos/directconn/receiver/WifiP2PBroadcastReceiver.java
@@ -1,25 +1,37 @@
 package br.com.kalffman.projetos.directconn.receiver;
 
+import android.Manifest;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.wifi.p2p.WifiP2pDevice;
 import android.net.wifi.p2p.WifiP2pManager;
+
+import androidx.core.app.ActivityCompat;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import br.com.kalffman.projetos.directconn.MainActivity;
 
 public class WifiP2PBroadcastReceiver extends BroadcastReceiver {
+
     public interface WifiP2PChangeListener {
-        public void onChange(String action);
+        public void onChange(String action, List<WifiP2pDevice> devices);
     }
 
     private WifiP2pManager manager;
     private WifiP2pManager.Channel channel;
     private MainActivity activity;
 
-    public WifiP2PBroadcastReceiver(WifiP2pManager manager, WifiP2pManager.Channel channel, MainActivity activity) {
+    private WifiP2PChangeListener listener;
+
+    public WifiP2PBroadcastReceiver(WifiP2pManager manager, WifiP2pManager.Channel channel, MainActivity activity, WifiP2PChangeListener listener) {
         this.manager = manager;
         this.channel = channel;
         this.activity = activity;
+        this.listener = listener;
     }
 
     @Override
@@ -30,9 +42,26 @@ public class WifiP2PBroadcastReceiver extends BroadcastReceiver {
 
         switch (action) {
             case WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION:
+
+                int state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1);
+
+                if (state == WifiP2pManager.WIFI_P2P_STATE_ENABLED) {
+                    this.listener.onChange("Conexão P2P habilitado", null);
+                } else {
+                    this.listener.onChange("Conexão P2P desabilitado", null);
+                }
+                break;
             case WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION:
+                if (activity != null) {
+                    if (ActivityCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) return;
+
+                    manager.requestPeers(channel, peerListListener);
+                }
+                break;
             case WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION:
             case WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION:
         }
     }
+
+    private final WifiP2pManager.PeerListListener peerListListener = peers -> listener.onChange(null, new ArrayList<>(peers.getDeviceList()));
 }
